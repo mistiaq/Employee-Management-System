174efd3a071998a90532a4c1dd26613e4865543e
IstiaqOsman <istiaq.workmail@gmail.com>
2022-01-09 21:20:00 +0600

    Created demo main page

---
+++ lib/demo4all.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class demo_for_all extends StatefulWidget{
+  @override
+  _demo_for_all_state createState() => _demo_for_all_state();
+}
+
+class _demo_for_all_state extends State<demo_for_all> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Colors.red,
+    );
+  }
+}
